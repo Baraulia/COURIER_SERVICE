1f46f3a2cc5aa3181fca35cef7880de05333255f
polina <nalivayko0428@gmail.com>
2022-02-23 23:10:46 +0300

    feat: add tests for controller

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/controller"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/dao"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/model"
@@ -14,20 +15,21 @@ import (
 func main() {
 	log.Println("Start...")
 	db, err := dao.NewPostgresDB(dao.PostgresDB{
-		"159.223.1.135",
-		"5434",
-		"courierteam1",
-		"qwerty",
-		"courier_db",
-		"disable"})
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_DATABASE"),
+		SSLMode:  os.Getenv("DB_SSL_MODE")})
 	if err != nil {
 		log.Println("failed to initialize db:", err.Error())
 	}
 	repos := dao.NewRepository(db)
 	services := model.NewService(repos)
 	handlers := controller.NewHandler(services)
+	host := os.Getenv("API_SERVER_PORT")
 	s := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + host,
 		Handler: handlers.InitRoutesGin(),
 	}
 	err = s.ListenAndServe()
