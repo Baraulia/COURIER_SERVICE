809c183fa9b112739d39857d1dc921684d775ac8
polina <nalivayko0428@gmail.com>
2022-02-23 19:44:26 +0300

    fix: use environment variables

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/controller"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/dao"
 	"stlab.itechart-group.com/go/food_delivery/courier_service/service"
@@ -14,21 +15,21 @@ import (
 func main() {
 	log.Println("Start...")
 	db, err := dao.NewPostgresDB(dao.PostgresDB{
-		"159.223.1.135",
-		"5434",
-		"courierteam1",
-		"qwerty",
-		"courier_db",
-		"disable"})
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_DATABASE"),
+		SSLMode:  os.Getenv("DB_SSL_MODE")})
 	if err != nil {
 		log.Fatal("failed to initialize db:", err.Error())
 	}
 	repos := dao.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := controller.NewHandler(services)
-	//host:=os.Getenv("API_SERVER_PORT")
+	host := os.Getenv("API_SERVER_PORT")
 	s := &http.Server{
-		Addr:    ":8080", // ":"+host,
+		Addr:    ":" + host,
 		Handler: handlers.InitRoutesGin(),
 	}
 	err = s.ListenAndServe()
