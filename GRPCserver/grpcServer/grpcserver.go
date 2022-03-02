90068e6a50d13f559eb87c81d004a96171050245
Timofei <limjkeee@mail.ru>
2022-03-02 13:59:05 +0300

    Refactored task FDS-71 for develop

---
+++ GRPCserver/grpcServer/grpcserver.go
@@ -0,0 +1,46 @@
+package grpcServer
+
+import (
+	"context"
+	"fmt"
+	courierProto "github.com/Baraulia/COURIER_SERVICE/GRPCserver"
+	"github.com/Baraulia/COURIER_SERVICE/service"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"net"
+)
+
+type GRPCServer struct {
+	service *service.Service
+	courierProto.UnimplementedCourierServerServer
+}
+
+func NewGRPCServer(service *service.Service) {
+	s := grpc.NewServer()
+	str := &GRPCServer{service: service}
+	courierProto.RegisterCourierServerServer(s, str)
+	lis, err := net.Listen("tcp", ":8091")
+	if err != nil {
+		logrus.Fatalf("NewGRPCServer, Listen:%s", err)
+	}
+	reflection.Register(s)
+	if err = s.Serve(lis); err != nil {
+		logrus.Fatalf("NewGRPCServer, Serve:%s", err)
+	}
+
+}
+
+func (g *GRPCServer) CreateOrder(ctx context.Context, order *courierProto.OrderCourierServer) (*emptypb.Empty, error) {
+	return nil, g.service.OrderApp.CreateOrder(order)
+}
+
+func (g *GRPCServer) GetDeliveryService(ctx context.Context, in *emptypb.Empty) (*courierProto.ServiceResponse, error) {
+	res, err := g.service.OrderApp.GetServices()
+	if err != nil {
+		logrus.Errorf("GetService:%s", err)
+		return nil, fmt.Errorf("GetService:%w", err)
+	}
+	return res, nil
+}
--- go.mod
+++ go.mod
@@ -7,10 +7,13 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/golang/mock v1.6.0
 	github.com/lib/pq v1.10.4
+	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.4.1
 	github.com/swaggo/swag v1.7.9
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
 )
 
 require (
@@ -26,7 +29,7 @@ require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
@@ -41,6 +44,7 @@ require (
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
