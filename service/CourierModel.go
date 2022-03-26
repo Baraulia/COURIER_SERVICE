e786a5e48c2265fc96678456dc466c8c6fac4466
polina <nalivayko0428@gmail.com>
2022-03-26 18:39:26 +0300

    fix: UpdateCourier

--- service/CourierModel.go
+++ service/CourierModel.go
@@ -60,15 +60,6 @@ func (s *CourierService) SaveCourier(courier *dao.Courier) (*dao.Courier, error)
 }
 
 func (s *CourierService) UpdateCourier(id uint16, status bool) (uint16, error) {
-	get, err := s.repo.GetCourierFromDB(int(id))
-	if (get == dao.Courier{}) {
-		return 0, fmt.Errorf("Error in CourierService: %s", err)
-	}
-	if id == 0 {
-		err := errors.New("no id")
-		log.Println("id cannot be zero")
-		return 0, fmt.Errorf("Error in CourierService: %s", err)
-	}
 	courierId, err := s.repo.CourierRep.UpdateCourierInDB(id, status)
 	if err != nil {
 		return 0, fmt.Errorf("Error with database: %s", err)
